a50f991fa26fa71f4dca5a9bf66e115cb0f75660
DavidSC <david@wbcboxing.com>
2020-11-01 23:13:04 -0600

    terminando el proyecto en el JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,36 @@
 //Boton flotante en el footer
 document.addEventListener('DOMContentLoaded', () => {
 
+    //Obtener la imagen
+
+    const imagenHero = document.querySelector('.hero');
+
+    //Inicialiar las variables
+    let i = 0;
+    let tiempo = 0;
+
+    //Arreglo con imagenes de fondo
+    const imagenes = ['arriba2.jpg', 'arriba.jpg'];
+
+
+
+    setInterval(() => {
+        imagenHero.style.backgroundPositionY = '-' + tiempo + 'px';
+
+        if (tiempo > 30) {
+            tiempo = 0;
+            imagenHero.style.backgroundImage = "url(../img/" + imagenes[i] + ")";
+
+            if (i === imagenes.length - 1) { //llego a la ultima imagen
+                i = 0; //reinicia conn la primer imagen
+            } else {
+                i++;
+            }
+        }
+        tiempo++;
+        // console.log(tiempo);
+    }, 100);
+
     const btnFlotante = document.querySelector('.btn-flotante');
 
     btnFlotante.addEventListener('click', (e) => {
